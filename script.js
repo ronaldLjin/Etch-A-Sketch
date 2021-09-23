cc00546f0bda3e96f8b6b10829dc3c181583ed58
ronaldLjin <ronyjin123@gmail.com>
2021-09-22 19:44:49 -0600

    Added many features

--- script.js
+++ script.js
@@ -1,43 +1,88 @@
 const gridArea = document.querySelector("#grid");
 let gridAreaStyle = document.getElementsByClassName('grid')
 const gridLine = document.querySelector('.gridline')
-gridLine.textContent = "No Grid-line";
+gridLine.textContent = "Grid-line On";
 const clear = document.querySelector('#clear')
+let pixelSlider = document.querySelector('#pixelSlider')
+let output = document.querySelector('#demo')
+pixelSlider.onchange = (e) => pixelSliderChange(e.target.value)
+let gridSquare = [];
+const random = document.querySelector('#random')
+window.addEventListener("load", startup, true)
+let cellColor = '#000000';
+const eraser = document.getElementById('eraser')
+
+pixelSliderChange = function(value) {
+    output.innerHTML = `${value} x ${value}`;
+    gridArea.innerHTML = '';
+    if (gridLine.textContent == 'Grid-line Off') {
+        gridSize(value);
+        gridArea.style.gridGap = '1px';
+    } else {
+        gridSize(value);
+    }
+}
+
 
 function gridSize(width) {
-    let gridSquare = [];
     for (i = 0; i < width * width; ++i) {
         gridSquare[i] = document.createElement("div");
         gridSquare[i].classList.add('cell')
         gridArea.appendChild(gridSquare[i])
     }
-    gridArea.style.cssText = `display: grid; grid-template-columns: repeat(${width}, auto); background-color: black; grid-gap: 1px`;
+    gridArea.style.cssText = `display: grid; grid-template-columns: repeat(${width}, auto); background-color: #c5c5c5`;
 
     for (i = 0; i < gridSquare.length; i++) {
         gridSquare[i].addEventListener("mouseover", changeColor(i))
     }
 
+    random.addEventListener("click", () => {
+        for (i = 0; i < gridSquare.length; i++) {
+            gridSquare[i].addEventListener("mouseover", () => {
+                randomColor = Math.floor(Math.random() * 16777215).toString(16);
+                cellColor = '#' + randomColor;
+            })
+        }
+    })
+
     function changeColor(i) {
         return function() {
-            gridSquare[i].style.cssText = 'background-color: black'
+            gridSquare[i].style.backgroundColor = `${cellColor}`
         };
     }
 
-    gridLine.addEventListener("click", () => {
-        if (gridLine.textContent == 'No Grid-line') {
-            gridArea.style.cssText = `display: grid; grid-template-columns: repeat(${width}, auto); background-color: black; grid-gap: 0px`;
-            gridLine.textContent = "Grid-line";
-        } else {
-            gridArea.style.cssText = `display: grid; grid-template-columns: repeat(${width}, auto); background-color: black; grid-gap: 1px`;
-            gridLine.textContent = "No Grid-line"
-        }
-    });
+    eraser.addEventListener('click', functionOne)
 
-    clear.addEventListener("click", () => {
-        for (i = 0; i < gridSquare.length; i++) {
-            gridSquare[i].style.cssText = 'background-color: white'
-        }
-    })
+    function functionOne() {
+        cellColor = 'white';
+    }
 }
+
+clear.addEventListener("click", () => {
+    for (i = 0; i < gridSquare.length; i++) {
+        gridSquare[i].style.cssText = 'background-color: white'
+    }
+})
+
+function watchColorPicker(event) {
+    cellColor = event.target.value;
+}
+
+function startup() {
+    colorPicker = document.querySelector('#colorpicker')
+    colorPicker.value = "#000000";
+    colorPicker.addEventListener("input", watchColorPicker, true);
+}
+
+gridLine.addEventListener("click", () => {
+    if (gridLine.textContent == 'Grid-line Off') {
+        gridArea.style.gridGap = '0px';
+        gridLine.textContent = 'Grid-line On';
+    } else {
+        gridArea.style.gridGap = '1px';
+        gridLine.textContent = 'Grid-line Off';
+    }
+});
+
 
 gridSize(25)
